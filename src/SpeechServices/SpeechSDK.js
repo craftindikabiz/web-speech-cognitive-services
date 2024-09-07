b72cf665b1f2e9fed61b19909e3adb7f841315b5
Anubhav Singh <anubhav@mail.craftindika.com>
2024-09-08 01:20:00 +0530

    init

---
+++ src/SpeechServices/SpeechSDK.js
@@ -0,0 +1,17 @@
+// We are only importing what we need.
+
+import {
+  AudioConfig,
+  OutputFormat,
+  ResultReason,
+  SpeechConfig,
+  SpeechRecognizer
+} from 'microsoft-cognitiveservices-speech-sdk/distrib/lib/microsoft.cognitiveservices.speech.sdk';
+
+export default {
+  AudioConfig,
+  OutputFormat,
+  ResultReason,
+  SpeechConfig,
+  SpeechRecognizer
+};
---
+++ src/SpeechServices/TextToSpeech/AudioContextConsumer.js
@@ -0,0 +1,42 @@
+/* eslint no-await-in-loop: "off" */
+
+export default class {
+  constructor(audioContext) {
+    this.audioContext = audioContext;
+  }
+
+  pause() {
+    this.audioContext && this.audioContext.suspend();
+    this.playingUtterance && this.playingUtterance.dispatchEvent(new CustomEvent('pause'));
+  }
+
+  resume() {
+    this.audioContext && this.audioContext.resume();
+    this.playingUtterance && this.playingUtterance.dispatchEvent(new CustomEvent('resume'));
+  }
+
+  async start(queue) {
+    let utterance;
+
+    while ((utterance = queue.shift())) {
+      this.playingUtterance = utterance;
+
+      await utterance.play(this.audioContext);
+
+      this.playingUtterance = null;
+    }
+  }
+
+  stop() {
+    this.playingUtterance && this.playingUtterance.stop();
+
+    if (this.audioContext.state === 'suspended') {
+      // Play -> Pause -> Cancel (stop)
+      // This would generate these events: "start", "pause", "end"
+
+      // Without this code, the "end" event will not emit until resume() is called
+      // Cancelling an unstarted utterance will not emit any "start" or "end" event
+      this.audioContext.resume();
+    }
+  }
+}
